fd6479f42f0c9b95616f98dd9a404e7ff583218f
Daniil <set2901@mail.ru>
2019-06-10 22:49:20 +0300

    Внес необходимые корректировки.

---
+++ js/script.js
@@ -0,0 +1,44 @@
+var openFeedbackFormBtn = document.querySelector(".write-us-btn");
+var feedbackForm = document.querySelector(".feedback-form");
+var closeBtn = document.querySelectorAll(".modal-close");
+var openMapPopup = document.querySelector(".min-map-img");
+var mapPopup = document.querySelector(".map-container");
+var openCartPopupBtns = document.querySelectorAll(".buy-btn");
+var cartPopup = document.querySelector(".cart-popup");
+
+closeBtn.forEach(function(item){
+  item.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    feedbackForm && feedbackForm.classList.add("visually-hidden")
+    feedbackForm && feedbackForm.classList.remove("show-popup-animation");
+    mapPopup && mapPopup.classList.add("visually-hidden");
+    mapPopup && mapPopup.classList.remove("show-popup-animation");
+    cartPopup.classList.add("visually-hidden");
+    cartPopup.classList.remove("show-popup-animation");
+  });
+});
+
+if (openFeedbackFormBtn) {
+  openFeedbackFormBtn.addEventListener("click", function (evt) {
+   evt.preventDefault();
+   feedbackForm.classList.remove("visually-hidden");
+   feedbackForm.classList.add("show-popup-animation");
+  });
+}
+
+if (openMapPopup) {
+  openMapPopup.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    mapPopup.classList.remove("visually-hidden");
+    mapPopup.classList.add("show-popup-animation");
+  });
+}
+
+openCartPopupBtns.forEach(function(item){
+  item.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    cartPopup.classList.remove("visually-hidden");
+    cartPopup.classList.add("show-popup-animation");
+  });
+}) 
+
